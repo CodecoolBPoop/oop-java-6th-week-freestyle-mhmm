672da3de6f72933096c59e35fbed39707b8edda3
Szabolcs Gyorki <sz.gyorki@gmail.com>
2018-08-28 14:28:54 +0200

    development hotfixes

--- src/map/LevelOne.java
+++ src/map/LevelOne.java
@@ -1,11 +1,12 @@
 package map;
 
+import GameObjects.GameObject;
+
 public class LevelOne extends Level{
 
     public LevelOne(){
         HEIGHT = 8;
         WIDTH = 8;
-        this.map = new Character[HEIGHT][WIDTH];
-        this.map[][]
+        this.map = new GameObject[HEIGHT][WIDTH];
     }
 }

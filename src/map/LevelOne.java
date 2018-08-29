d209ee2279afa629e72cc8dc3797a30996f084e9
Fábián János <fabian.janos93@gmail.com>
2018-08-29 13:36:53 +0000

    hmm.. interesting

--- src/map/LevelOne.java
+++ src/map/LevelOne.java
@@ -3,6 +3,7 @@ import static GameObjects.GameObjectType.FLOOR;
 import static GameObjects.GameObjectType.FOREST;
 
 import GameObjects.Characters.Player;
+import GameObjects.Characters.enemy.Slime;
 import GameObjects.Floor;
 import GameObjects.GameObject;
 import GameObjects.GameObjectType;
@@ -18,13 +19,18 @@ public class LevelOne extends Level{
 
 
         for(int i = 0; i< HEIGHT; i++){
-            for (int j = 0; j<WIDTH; j++) {
-                this.map[i][j] = new Wall(i, j, FOREST);
+            for (int j = 1; j<WIDTH-1; j++) {
+                this.map[i][j] = new Floor(i, j, FLOOR);
             }
-
+            this.map[0][i] = new Wall(0,i,FOREST);
+            this.map[i][0] = new Wall(i,0,FOREST);
         }
-
         this.map[1][1] = player;
+        player.place(1,1);
+
+        this.map[4][4] = new Slime(4,4);
+        this.map[3][4] = new Slime(3,4);
+        this.map[2][4] = new Slime(2,4);
     }
 
     public GameObject[][] getLevel(){

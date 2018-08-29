9d662bb813d3db95d108ac19277befbce543d959
Fábián János <fabian.janos93@gmail.com>
2018-08-29 09:19:15 +0000

    character stats implemented

--- src/GameObjects/Characters/Enemy/Player.java
+++ src/GameObjects/Characters/Enemy/Player.java
@@ -1,5 +1,31 @@
 package GameObjects.Characters.Enemy;
 
 public class Player extends Character {
+    int strength;
+    int agility;
+    int intelligence;
+    Armor fullBody;
+    Weapon weapon;
+
+    public boolean strengthTest(int testValue){
+        return testValue<strength;
+    }
+
+    public boolean agilityTest(int testValue){
+        return testValue<agility;
+    }
+
+    public boolean intelligenceTest(int testValue){
+        return testValue<intelligence;
+    }
+
+    public void equip(Armor armor){
+        this.fullBody = armor;
+    }
+
+    public void equip(Weapon weapon){
+        this.weapon = weapon;
+    }
+
 
 }

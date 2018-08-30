b8ff642bea472285f244a66f3487e56b7b8d6559
Szabolcs Gyorki <sz.gyorki@gmail.com>
2018-08-30 09:29:10 +0200

    Moved logging from init to the dao class

--- src/Config.java
+++ src/Config.java
@@ -1,11 +1,7 @@
 import GameObjects.Items.Armor;
 import GameObjects.Items.Weapon;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Config {
-
-    private static final Logger logger = LoggerFactory.getLogger(Config.class);
 
     public static void initialize() {
 
@@ -17,24 +13,16 @@ public class Config {
         Armor armor4 = new Armor("Diamond Mail", 80, 40);
 
         itemsData.addItem(armor1);
-        logger.info("'{}' added to items database", armor1.getName());
         itemsData.addItem(armor2);
-        logger.info("'{}' added to items database", armor2.getName());
         itemsData.addItem(armor3);
-        logger.info("'{}' added to items database", armor3.getName());
         itemsData.addItem(armor4);
-        logger.info("'{}' added to items database", armor4.getName());
 
         Weapon weapon1 = new Weapon("Mythical Sword", 40, 40, 30);
         Weapon weapon2 = new Weapon("Colossus Blade", 40, 40, 30);
         Weapon weapon3 = new Weapon("Hydra Bow", 40, 40, 30);
 
         itemsData.addItem(weapon1);
-        logger.info("'{}' added to characters database", weapon1.getName());
         itemsData.addItem(weapon2);
-        logger.info("'{}' added to characters database", weapon2.getName());
         itemsData.addItem(weapon3);
-        logger.info("'{}' added to characters database", weapon3.getName());
     }
-
 }
--- src/ItemsDataDao.java
+++ src/ItemsDataDao.java
@@ -6,13 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ItemsDataDao implements GameObjectDAO {
 
     private static List<Item> itemList = new ArrayList<>();
-
     private static ItemsDataDao instance = null;
-
+    private static final Logger logger = LoggerFactory.getLogger(Config.class);
     private ItemsDataDao() {}
 
     public static ItemsDataDao getInstance() {
@@ -24,12 +25,12 @@ public class ItemsDataDao implements GameObjectDAO {
 
     public void addItem(Item item) {
         itemList.add(item);
+        logger.info("'{}' added to items database", item.getName());
     }
 
     @Override
     public void remove(int id) {
         itemList.remove(id);
-
     }
 
     public Item findById(int id) {
